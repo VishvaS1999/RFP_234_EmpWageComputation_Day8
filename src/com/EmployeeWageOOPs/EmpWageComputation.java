b7be57b37edbabf69a1a760a4f17fb2fd950e167
VishvaS1999 <gvishva2601@gmail.com>
2022-12-13 22:41:24 +0530

    Calculate Daily Employee Wage

--- src/com/EmployeeWageOOPs/EmpWageComputation.java
+++ src/com/EmployeeWageOOPs/EmpWageComputation.java
@@ -1,27 +1,40 @@
 package com.EmployeeWageOOPs;
 
-class Attendance
+class EmpCalculation
 {
-    public static final int IS_FULL_TIME = 1;
+    public static int empHrs;
     public void empCheck()
     {
         int empCheck = (int)Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
+        if(empCheck == 1)
         {
-            System.out.println("Employee Is Present");
+            System.out.println("Employee is present");
+            empHrs = 8;
         }
         else
         {
-            System.out.println("Employee Is Absent");
+            System.out.println("Employee is absent");
         }
+    }
+}
+class DailyWage
+{
+    public static final int EMP_RATE_PER_HOUR = 20;
+
+    public void dailyWageCalculation()
+    {
+        int totalDailyWage = EmpCalculation.empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Daily employee wage is "+totalDailyWage);
     }
 }
 public class EmpWageComputation
 {
     public static void main(String[] args)
     {
-        Attendance attendance = new Attendance();
-        attendance.empCheck();
+        EmpCalculation emp = new EmpCalculation();
+        emp.empCheck();
+        DailyWage wage = new DailyWage();
+        wage.dailyWageCalculation();
     }
 }
 
